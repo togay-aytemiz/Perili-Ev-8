1c458e7a8353337d4899fb29b644dc0da68a1c38
togay-aytemiz <togayaytemiz@gmail.com>
2024-02-16 20:56:24 +0300

    first commit

---
+++ src/components/housecard.jsx
@@ -0,0 +1,64 @@
+const HouseCard = ({houseData, index, array}) => {
+    return (
+      <div className="house-card" key={houseData.id}>
+        <p>
+          {array.length} / {index + 1}
+        </p>
+        <img src={houseData.image} />
+        <div>
+          <ul>
+            <li>
+              <span>Fiyat:</span>
+              {houseData.price}
+            </li>
+            <li>
+              <span>Lokasyon:</span>
+              {houseData.location}
+            </li>
+            <li>
+              <span>Metrekare:</span>
+              {houseData.squareFeet}
+            </li>
+            <li>
+              <span>Dönüm:</span>
+              {houseData.acres}
+            </li>
+            <li>
+              <span>Yapım Yılı:</span>
+              {houseData.yearBuilt}
+            </li>
+            <li>
+              <span>Yatak Odası:</span>
+              {houseData.bedrooms}
+            </li>
+            <li>
+              <span>Banyo:</span>
+              {houseData.bathrooms}
+            </li>
+            <li>
+              <span>Diğer Odalar:</span>
+              {houseData.otherRooms}
+            </li>
+            <li>
+              <span>Garaj:</span>
+              {houseData.garage ? "Evet" : "Hayır"}
+            </li>
+            <li>
+              <span>Klima:</span>
+              {houseData.airConditioning ? "Evet" : "Hayır"}
+            </li>
+            <li>
+              <span>Isıtma Sistemi:</span>
+              {houseData.heating ? "Evet" : "Hayır"}
+            </li>
+            <li>
+              <span>Lanetli:</span>
+              {houseData.haunted ? "Evet" : "Hayır"}
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+export default HouseCard
